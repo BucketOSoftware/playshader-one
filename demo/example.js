6adf7356763a22c9e80d73ae2a1b69dcdb0b642c
M. Zole <zole@zole.org>
2022-05-24 16:31:12 -0700

    Make resource loading slightly more efficient, maybe

--- demo/example.js
+++ demo/example.js
@@ -1,150 +1,167 @@
-const SKY_COLOR = 0x3c9f9c;
-const RESOLUTION = [320, 240];
-let gl, scene, camera, light, ambient;
-let house;
-let lastTime = 0;
-let totalTime = 0;
+/*
+ * playshader-one demo
+ *
+ * IMPORTANT NOTE: this code is designed to be straightforward (no build
+ * tooling, etc.), not to demonstrate best practices, whatever those are. Had
+ * this been a real application, I would have written better organized code.
+ */
+const SKY_COLOR = 0x3c9f9c
+const RESOLUTION = [320, 240]
+let gl, scene, camera, light, ambient
 
-const itemsToLoad = 7;
+let loading, house
+
+let lastTime = 0
+let totalTime = 0
+
+const itemsToLoad = 7
 function loadingProgress(url, itemsLoaded, itemsTotal) {
-  // console.log("Loading:", url, itemsLoaded, itemsTotal);
-  console.log("Progress: %.0f%%", itemsLoaded / itemsToLoad * 100);
+    // console.log("Loading:", url, itemsLoaded, itemsTotal);
+    console.log('Progress: %.0f%%', (itemsLoaded / itemsToLoad) * 100)
+}
+
+async function fetchShader(type) {
+    const file = await fetch(`playshader.${type}`)
+    return file.text()
 }
 
 async function main() {
-  // Create the shader
+    document.body.style.backgroundColor = `#${SKY_COLOR.toString(16)}`
 
-  const psx = new THREE.ShaderMaterial({
-    lights: true,
-    fog: true,
-    uniforms: THREE.UniformsUtils.merge([
-      THREE.UniformsLib.lights,
-      THREE.UniformsLib.fog,
-      {
-        // Commonly used resolution for the hardware
-        resolution: { value: RESOLUTION },
-        map: { value: null },
-      },
-    ]),
-    defines: {
-      // NO_DITHERING: 1,
-    },
-    glslVersion: THREE.GLSL3,
-    vertexShader: null,
-    fragmentShader: null,
-  });
+    // Start loading resources
+    
+    const loader = new THREE.GLTFLoader()
+    loader.manager.onStart = loadingProgress
+    loader.manager.onProgress = loadingProgress
 
-  gl = new THREE.WebGLRenderer({
-    antialias: false,
-  });
-  gl.outputEncoding = THREE.sRGBEncoding;
-  // gl.setPixelRatio(4)
+    const resourcesPromise = Promise.all([
+        loader.loadAsync('nekostop.gltf'),
+        ['vert, frag'].map((t) => fetchShader(t)),
+    ])
 
-  // gl.setSize(window.innerWidth, window.innerHeight)
-  gl.setSize(...RESOLUTION);
-  gl.setPixelRatio(window.devicePixelRatio * 3);
-  gl.domElement.style.width = "640px";
-  gl.domElement.style.height = "480px";
-  gl.domElement.style.imageRendering = "pixelated";
-  document.body.appendChild(gl.domElement);
+    // Create the shader
 
-  // Scene
-
-  scene = new THREE.Scene();
-  scene.fog = new THREE.Fog(SKY_COLOR, 15, 40);
-  scene.background = new THREE.Color(SKY_COLOR);
-
-  // Lighting
-  
-  const ambient = new THREE.AmbientLight(0x404040);
-  scene.add(ambient);
-
-  light = new THREE.DirectionalLight(0xffffff, 5);
-  light.position.set(0, 20, 5);
-  light.target.position.set(0, 0, 0);
-  scene.add(light);
-
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-
-  scene.add(cube);
-
-  camera = new THREE.PerspectiveCamera(60, 4 / 3, 1, 1000);
-  camera.position.set(0, 9, 14);
-  camera.lookAt(0, 3, 0);
-
-  // Render while we wait for the model to load
-  // TODO: display a logo perhaps
-  // TODO: display a progress bar
-  requestAnimationFrame(render);
-
-  const loader = new THREE.GLTFLoader();
-  loader.manager.onStart = loadingProgress;
-  loader.manager.onProgress = loadingProgress;
-
-  const [model, vert, frag] = await Promise.all([
-    loader.loadAsync("nekostop/nekostop.gltf"),
-    fetch("playshader.vert"),
-    fetch("playshader.frag"),
-  ]);
-  // debugger
-  [psx.vertexShader, psx.fragmentShader] = await Promise.all([
-    vert.text(),
-    frag.text(),
-  ]);
-
-  console.dir(model);
-  // model.scene.scale.set(0.1, 0.1, 0.1)
-  /*
-    model.scene.traverse(obj => {
-        console.dir(obj)
+    const psx = new THREE.ShaderMaterial({
+        lights: true,
+        fog: true,
+        uniforms: THREE.UniformsUtils.merge([
+            THREE.UniformsLib.lights,
+            THREE.UniformsLib.fog,
+            {
+                // Commonly used resolution for the hardware
+                resolution: { value: RESOLUTION },
+                map: { value: null },
+            },
+        ]),
+        defines: {
+            // NO_DITHERING: 1,
+        },
+        glslVersion: THREE.GLSL3,
+        vertexShader: null,
+        fragmentShader: null,
     })
-*/
-  house = model.scene.getObjectByName("Scene");
-  // house.scale.set(0.1, 0.1, 0.1)
 
-  // Apply the PlayShader One material
-  house.traverse((obj) => {
-    if (obj.name === "lantern") {
-      /*
-      const ptLight = new THREE.PointLight(0x0033ff, 10, 2, 1);
-      ptLight.position.copy(obj.position);
-      obj.parent.add(ptLight);
-      */
-    }
-    if (obj.material && true) {
-      const oldMaterial = obj.material;
-      obj.material = psx.clone();
-      obj.material.uniforms.map.value = oldMaterial.map;
-      oldMaterial.dispose();
-    }
-  });
+    gl = new THREE.WebGLRenderer({
+        antialias: false,
+        // alpha: true,
+    })
+    gl.outputEncoding = THREE.sRGBEncoding
+    // gl.setPixelRatio(4)
 
-  scene.remove(cube);
-  scene.add(model.scene);
+    // gl.setSize(window.innerWidth, window.innerHeight)
+    gl.setSize(...RESOLUTION)
+    // gl.setPixelRatio(window.devicePixelRatio * 3);
+    gl.domElement.style.width = '640px'
+    gl.domElement.style.height = '480px'
+    // Ask the browser to upscale in a chunky fashion
+    gl.domElement.style.imageRendering = 'pixelated'
+    document.body.appendChild(gl.domElement)
 
-  lastTime = performance.now();
-  requestAnimationFrame(render);
+    // Scene
+
+    scene = new THREE.Scene()
+    scene.fog = new THREE.Fog(SKY_COLOR, 15, 40)
+    scene.background = new THREE.Color(SKY_COLOR)
+
+    // Lighting
+
+    const ambient = new THREE.AmbientLight(0x404040)
+    scene.add(ambient)
+
+    light = new THREE.DirectionalLight(0xffffff, 5)
+    light.position.set(0, 20, 5)
+    light.target.position.set(0, 0, 0)
+    scene.add(light)
+
+    // Camera
+
+    camera = new THREE.PerspectiveCamera(60, 4 / 3, 1, 1000)
+    camera.position.set(0, 9, 14)
+    camera.lookAt(0, 3, 0)
+
+    // Render something while we wait for assets to load
+    // TODO: display the logo and/or progress bar
+
+    const geometry = new THREE.BoxGeometry()
+    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+    const loadModel = new THREE.Mesh(geometry, material)
+    scene.add(loadModel)
+
+    requestAnimationFrame(render)
+
+
+
+    const [model, vert, frag] = await resourcesPromise;
+ 
+    psx.vertexShader = vert
+    psx.fragmentShader = frag
+
+    // console.dir(model)
+
+    house = model.scene.getObjectByName('Scene')
+
+    // Apply the PlayShader One material
+    house.traverse((obj) => {
+        if (obj.name === 'lantern') {
+            // TODO: find a way to make it look like this is lighting up the lantern, perhaps?
+            const ptLight = new THREE.PointLight(0x0033ff, 10, 2, 1);
+            ptLight.position.copy(obj.position);
+            obj.parent.add(ptLight);
+        }
+        if (obj.material && true) {
+            const oldMaterial = obj.material
+            obj.material = psx.clone()
+            obj.material.uniforms.map.value = oldMaterial.map
+            oldMaterial.dispose()
+        }
+    })
+
+    scene.remove(cube)
+    scene.add(model.scene)
+
+    lastTime = performance.now()
+    requestAnimationFrame(render)
 }
+
 
 const lightMoveSpeed = 50
 function render(time) {
-  let delta = (time - lastTime) / 1000;
-  if (delta > 1) { delta = 0.1; }
-  totalTime += delta;
-  lastTime = time;
+    let delta = (time - lastTime) / 1000
+    if (delta > 1) {
+        delta = 0.1
+    }
+    totalTime += delta
+    lastTime = time
 
-  // console.log(delta)
-  if (house) {
-    requestAnimationFrame(render);
-    house.rotateY(Math.PI * 0.05 * delta);
-    // console.log(totalTime / lightMoveSpeed)
-    light.position.x = Math.sin(totalTime / lightMoveSpeed) * 30;
-    light.position.z = Math.cos(totalTime / lightMoveSpeed) * 5;
-    // light.position.set(totalTime / 500, 20, 0);
-  }
-  gl.render(scene, camera);
+    if (house) {
+        requestAnimationFrame(render)
+        house.rotateY(Math.PI * 0.05 * delta)
+        light.position.x = Math.sin(totalTime / lightMoveSpeed) * 30
+        light.position.z = Math.cos(totalTime / lightMoveSpeed) * 5
+    } else if (loadModel) {
+        loadModel.rotateY(Math.PI * 2 * delta)
+    }
+    gl.render(scene, camera)
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main)
